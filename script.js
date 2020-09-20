7b5724c9e3ffc27187e71fd40bb983fcf95579fb
Matapeja <48495523+Matapeja@users.noreply.github.com>
2020-09-20 12:39:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function bulbOn() {
+    document.getElementById("bulb").src = "bulbon.png";
+    document.querySelector("body").style.background = "#fff148";
+}
+
+function bulbOff() {
+    document.getElementById("bulb").src = "bulboff.png";
+    document.querySelector("body").style.background = "#262626";
+}
